e3d6bd9403fea4b5da9bbb17905478929aed7d13
Robert Winslow <winslowr@Roberts-MBP-3.attlocal.net>
2021-02-28 15:54:55 -0500

    turned scheduling into a stateful widget, allowing multiple time periods to be added to each day

--- lib/ui/widgets/forms/caregiver_application/scheduler.widget.dart
+++ lib/ui/widgets/forms/caregiver_application/scheduler.widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:careyaya/ui/widgets/forms/caregiver_application/time_selector.widget.dart';
 
-class Scheduler extends StatelessWidget {
+/*class Scheduler extends StatelessWidget {
 
   final String day;
 
@@ -17,5 +17,45 @@ class Scheduler extends StatelessWidget {
       ElevatedButton(onPressed: null, child: Text("+")),
       SizedBox(height: 10),
     ]);
+  }
+}*/
+
+class Scheduler extends StatefulWidget {
+
+  final String day;
+
+  const Scheduler(this.day);
+
+  @override
+  _SchedulerState createState() => _SchedulerState();
+}
+
+class _SchedulerState extends State<Scheduler> {
+
+  int numPeriods = 1;
+  List<TimeSelector> timePeriods = <TimeSelector>[];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column( children: [
+      Text(widget.day),
+      ListView.builder(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          itemCount: this.timePeriods.length,
+          itemBuilder: (context, index) {
+            return this.timePeriods[index];
+          }
+      ),
+      ElevatedButton(onPressed: _handleAddTimePress, child: Text("+")),
+      SizedBox(height: 10),
+    ]);
+  }
+
+  _handleAddTimePress() {
+      setState(() {
+        this.timePeriods.add(TimeSelector(this.widget.day.toLowerCase(), this.numPeriods));
+        this.numPeriods = this.numPeriods + 1;
+      });
   }
 }

45fb320de1bffca4ecd57966cd0a116b2d0e0b44
pricelev@ad.unc.edu <pricelev@ad.unc.edu>
2021-03-16 10:35:25 -0400

    changed activeStep to 0

--- lib/ui/widgets/forms/caregiver_application/caregiver_application_form.widget.dart
+++ lib/ui/widgets/forms/caregiver_application/caregiver_application_form.widget.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class StepperController extends GetxController {
-  int activeStep = 3;
+  int activeStep = 0;
 
   void next() {
     activeStep++;

6a933c2e37573f5247fd772939c0fdc2f731b8d5
pricelev@ad.unc.edu <pricelev@ad.unc.edu>
2021-03-25 10:17:42 -0400

    Updated screen.detail.widget to have a better look, removed the picture, adjusted the location view

--- lib/ui/widgets/session/session.detail.widget.dart
+++ lib/ui/widgets/session/session.detail.widget.dart
@@ -12,76 +12,141 @@ class SessionDetailWidget extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return Column(
-        children:[
-          Center(
-            child: Container(
-                width: 70.0,
-                height: 70.0,
-                margin: const EdgeInsets.only(top: 20.0,bottom: 40),
 
-                decoration: new BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: new DecorationImage(
-                      fit: BoxFit.fill,
-                      image: NetworkImage('https://goheels.com/images/2015/6/2/CGUAZBDTMRQVCFP.20150602144508.jpg?width=1416&height=797&mode=crop&quality=80&format=jpg'),
-                    )
-                )
-            ),
-          ),
+        children:[
+          SizedBox(height:30),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text('Name'),
-              Text('${session.lovedOneId}'),
+              Text('Loved Ones Name:',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      decoration: TextDecoration.underline
+                  )),
+              Text('${session.lovedOneId}',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  )),
             ],
           ),
+          SizedBox(height:25),
           Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text('Status'),
+                Text('Status:',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        decoration: TextDecoration.underline
+                    )),
                 Chip(label: Text(session.accepted ? "Accepted" : "Requested")),
             ],
           ),
+          SizedBox(height:25),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text('Time'),
-              Text('${session.timeSlots[1]} - ${session.timeSlots[session.timeSlots.length-1]}')
+              Text('Time:',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      decoration: TextDecoration.underline
+                  )),
+              Text('${session.timeSlots[1]} - ${session.timeSlots[session.timeSlots.length-1]}',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ))
             ],
           ),
+          SizedBox(height:30),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text('Location'),
-              Text('${session.address.description}'),
+              Text('Location:',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  decoration: TextDecoration.underline
+                )),
+              Text('${session.address.line1} \n${session.address.city}, ${session.address.state} \n${session.address.zip} ' ,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.center,
+
+              ),
             ],
           ),
+          SizedBox(height:30),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text('Distance Away'),
-              Text('${session.distance}'),
+              Text('Distance Away:',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      decoration: TextDecoration.underline
+                  )),
+              Text('${session.distance}',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  )),
             ],
           ),
+          SizedBox(height:30),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text('Rate'),
-              Text('${session.hourlyRate} / HR'),
+              Text('Rate:',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      decoration: TextDecoration.underline
+                  )),
+              Text('${session.hourlyRate/100} / HR',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  )),
             ],
           ),
+          SizedBox(height:30),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text('Total Cost'),
-              Text('${session.totalCost}'),
+              Text('Total Cost:',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      decoration: TextDecoration.underline
+                  )),
+              Text('${session.totalCost/100} dollars',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  )),
             ],
           ),
+          SizedBox(height:30),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text('Payment Method'),
-              Text('${session.stripePaymentIntentId}'),
+              Text('Payment Method:',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      decoration: TextDecoration.underline
+                  )),
+              Text('${session.stripePaymentIntentId}',
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                  )),
             ],
           ),]
     );

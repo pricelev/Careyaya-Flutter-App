2efc54f585c548144ff6675f186e927b1e1b7328
pricelev@ad.unc.edu <pricelev@ad.unc.edu>
2021-03-26 22:09:40 -0400

    Created expansionTile to make the page look better

--- lib/ui/widgets/session/session.detail.widget.dart
+++ lib/ui/widgets/session/session.detail.widget.dart
@@ -8,23 +8,7 @@ class SessionDetailWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(children: [
-      SizedBox(height: 30),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text('Loved Ones Name:',
-              style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black,
-                  decoration: TextDecoration.underline)),
-          Text('${session.lovedOneId}',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.black,
-              )),
-        ],
-      ),
-      SizedBox(height: 25),
+      SizedBox(height:25),
       Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -36,97 +20,151 @@ class SessionDetailWidget extends StatelessWidget {
           Chip(label: Text(session.accepted ? "Accepted" : "Requested")),
         ],
       ),
-      SizedBox(height: 25),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text('Time:',
-              style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black,
-                  decoration: TextDecoration.underline)),
-          Text(
-            '${DateFormat('hh:mm aa').format(
-              DateTime.fromMillisecondsSinceEpoch(
-                  session.startTimestamp.millisecondsSinceEpoch),
-            )} - ${DateFormat('hh:mm aa').format(
-              DateTime.fromMillisecondsSinceEpoch(
-                  session.endTimestamp.millisecondsSinceEpoch),
-            )}',
+      ExpansionTile(
+        title: Text(
+          'Appointment Information',
             style: TextStyle(
-              fontSize: 16,
-              color: Colors.black,
-            ),
-          )
-        ],
-      ),
-      SizedBox(height: 30),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+            )
+        ),
         children: [
-          Text('Location:',
-              style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black,
-                  decoration: TextDecoration.underline)),
-          Text(
-            '${session.address.line1} \n${session.address.city}, ${session.address.state} \n${session.address.zip} ',
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.black,
-            ),
-            textAlign: TextAlign.center,
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Loved Ones Name:',
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                      decoration: TextDecoration.underline)),
+              Text('${session.lovedOneId}',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  )),
+            ],
           ),
-        ],
-      ),
-      SizedBox(height: 30),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text('Distance Away:',
-              style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black,
-                  decoration: TextDecoration.underline)),
-          Text('${session.distance}',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.black,
-              )),
-        ],
-      ),
-      SizedBox(height: 30),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text('Rate:',
-              style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black,
-                  decoration: TextDecoration.underline)),
-          Text('${session.hourlyRate / 100} / HR',
+          SizedBox(height: 20),
+          Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Time:',
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                    decoration: TextDecoration.underline)),
+            Text(
+              '${DateFormat('hh:mm aa').format(
+                DateTime.fromMillisecondsSinceEpoch(
+                    session.startTimestamp.millisecondsSinceEpoch),
+              )} - ${DateFormat('hh:mm aa').format(
+                DateTime.fromMillisecondsSinceEpoch(
+                    session.endTimestamp.millisecondsSinceEpoch),
+              )}',
               style: TextStyle(
                 fontSize: 16,
                 color: Colors.black,
-              )),
-        ],
+              ),
+            )
+          ],
+        ),
+          SizedBox(height: 5),]
       ),
-      SizedBox(height: 30),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text('Total Earnings:',
-              style: TextStyle(
-                  fontSize: 20,
+      ExpansionTile(
+        title: Text(
+            'Location Information',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            )
+        ),
+        children:[
+          SizedBox(height:10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Address:',
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                      decoration: TextDecoration.underline)),
+              Text(
+                '${session.address.line1} \n${session.address.city}, ${session.address.state} \n${session.address.zip} ',
+                style: TextStyle(
+                  fontSize: 16,
                   color: Colors.black,
-                  decoration: TextDecoration.underline)),
-          Text('${session.totalCost / 100} dollars',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.black,
-              )),
-        ],
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Distance Away:',
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                      decoration: TextDecoration.underline)),
+              Text('${session.distance}',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  )),
+            ],
+          ),
+          SizedBox(height: 5),
+        ]
       ),
+    ExpansionTile(
+      title: Text(
+          'Payment Information',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          )
+      ),
+      children: [
+        SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Rate:',
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                    decoration: TextDecoration.underline)),
+            Text('${session.hourlyRate / 100} / HR',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                )),
+          ],
+        ),
+        SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Total Earnings:',
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                    decoration: TextDecoration.underline)),
+            Text('${session.totalCost / 100} dollars',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                )),
+          ],
+        ),
+        SizedBox(height:5),
+      ],
+    )
     ]);
   }
 }

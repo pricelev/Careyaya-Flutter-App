0aeb1c2808e6c7da701be04ed29b06c42da44433
gavry <gavry.eshet@gmail.com>
2021-03-29 15:04:23 -0400

    Update firestore.dart

--- lib/constants/firestore.dart
+++ lib/constants/firestore.dart
@@ -4,4 +4,5 @@ const String CAREGIVER_LOCATIONS_COLLECTION = 'joygiverLocations';
 const String JOYGIVER_APPLICATIONS_COLLECTION = 'joygiverApplications';
 const String JOYGIVERS_COLLECTION = 'joygivers';
 const String SESSIONS_COLLECTION = 'sessions';
+const String SESSION_NOTES_BY_JOYGIVERS_COLLECTION = 'sessionNotesByJoygivers';
 const String ADVOCATES_COLLECTION = 'advocates';

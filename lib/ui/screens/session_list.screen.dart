24dee23ac7ac3da601a0d0b87b52e16e174469f0
pricelev@ad.unc.edu <pricelev@ad.unc.edu>
2021-03-30 14:40:50 -0400

    added tab viewer for seesion list page

--- lib/ui/screens/session_list.screen.dart
+++ lib/ui/screens/session_list.screen.dart
@@ -1,6 +1,7 @@
 import 'package:careyaya/models/sessions/session.model.dart';
 import 'package:careyaya/ui/screens/example_sessions.dart';
 import 'package:careyaya/ui/widgets/main_screen_layout.widget.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:careyaya/constants/routes.dart';
@@ -11,36 +12,88 @@ class SessionListScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MainScreenLayout(
       title: 'Sessions',
-      body: Column( children: [
-        Expanded(child: ListView.builder(
-            itemBuilder: (_, index) {
-              final hasBorderBottom =
-                  index == sessions.length - 1;
-              final boxDecoration = hasBorderBottom
-                  ? BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(
-                    color: Colors.grey,
-                    width: 0.0,
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children:[
+            DefaultTabController(
+              length:4,
+              initialIndex:0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                    child: TabBar(
+                      labelColor: Colors.blue,
+                      unselectedLabelColor: Colors.black,
+                      tabs: [
+                        Tab(text: 'upcoming'),
+                        Tab(text: 'requested'),
+                        Tab(text: 'past'),
+                        Tab(text: 'declined'),
+                      ]
+
+                    )
                   ),
-                ),
-                // borderRadius: BorderRadius.circular(5.0),
+                  Container(
+                    height: 400,
+                    decoration: BoxDecoration(
+                      border: Border(top: BorderSide(color: Colors.grey, width:.5))
+                    ),
+                    child:TabBarView(
+                      children:[
+                        Container(
+                          child:
+                            Text('coming soon')
+                        ),
+                        Column( children: [
+                          Expanded(child: ListView.builder(
+                              itemBuilder: (_, index) {
+                                final hasBorderBottom =
+                                    index == sessions.length - 1;
+                                final boxDecoration = hasBorderBottom
+                                    ? BoxDecoration(
+                                  border: Border(
+                                    bottom: BorderSide(
+                                      color: Colors.grey,
+                                      width: 0.0,
+                                    ),
+                                  ),
+                                  // borderRadius: BorderRadius.circular(5.0),
+                                )
+                                    : null;
+                                final session = sessions[index];
+                                return Container(
+                                  key: Key(session.joygiverId),
+                                  child: Card(child:
+                                  ListTile(
+                                    leading: Chip(label: Text(session.accepted ? "Accepted" : "Requested")),
+                                    title: Text('${session.hoursCount} hours with ${session.lovedOneId}'),
+                                    onTap: _onTap,
+                                  )),
+                                );
+                              },
+                              itemCount: sessions.length)),
+                        ]),
+                        Container(
+                            child:
+                            Text('coming soon')
+                        ),
+                        Container(
+                            child:
+                            Text('coming soon')
+                        ),
+                      ]
+                    )
+                  )
+                ]
               )
-                  : null;
-              final session = sessions[index];
-              return Container(
-                key: Key(session.joygiverId),
-                child: Card(child:
-                  ListTile(
-                    leading: Chip(label: Text(session.accepted ? "Accepted" : "Requested")),
-                    title: Text('${session.hoursCount} hours with ${session.lovedOneId}'),
-                    onTap: _onTap,
-                )),
-              );
-            },
-            itemCount: sessions.length)),
-      ]),
-    );
+            )
+          ]
+        ),
+      ),
+
+        );
   }
 
   void _onTap() {
